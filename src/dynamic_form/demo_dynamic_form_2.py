1e7358d485bb46a5c5ad69c152b0686b8fc0de94
Trang Truong <trang.truong@gigacover.com>
2020-04-12 17:46:53 +0700

    demo form

---
+++ src/dynamic_form/demo_dynamic_form_2.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField, IntegerField
+
+class demo_dynamic_form_2(FlaskForm):
+    First_Name = StringField('First Name', validators=[DataRequired()])
+    Last_Name = StringField('Last Name', validators=[DataRequired()])
+    Email = EmailField('Email', validators=[DataRequired()])
+    Address = StringField('Address', validators=[DataRequired()])
+    City = StringField('City', validators=[DataRequired()])
+    Province = StringField('Province', validators=[DataRequired()])
+    Postal_Code = IntegerField('Postal Code', validators=[DataRequired()])
+    Country = StringField('Country', validators=[DataRequired()])
+    Job_Title = StringField('Job Title', validators=[DataRequired()])
+    Contract_Duration = StringField('Contract Duration', validators=[DataRequired()])
+    Next = SubmitField('Next', validators=[DataRequired()])
+    
