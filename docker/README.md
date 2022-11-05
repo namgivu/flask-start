38b6d761564b9acb4b832717562277ca49a12ecd
Nam G VU <namgivu@gmail.com>
2022-11-05 14:47:57 +0700

    apiapp 0th

--- docker/README.md
+++
@@ -1,4 +0,0 @@
-# ref
-Dockerfile              ref. https://github.com/namgivu/falcon-start/blob/gc-intern-1910/Dockerfile
-docker-compose.yaml     ref. https://github.com/namgivu/falcon-start/blob/gc-intern-1910/docker-compose.yml
-docker-related util     ref. https://github.com/namgivu/falcon-start/tree/gc-intern-1910/bin/docker
--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/hi')
-def hi():
-   return 'Hi there!'
-
 @app.route('/')
 def index():
-   return render_template('index.html')
+   respbody = {
+      'abb': 122,
+      'ccc': 333,
+   }
+   return jsonify(respbody)
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000, debug=True)
